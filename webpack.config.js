4ff9e4799a707156794253d57684d457b92f20ff
Pravin Dahal <pravindahal@gmail.com>
2018-07-06 13:21:09 +0200

    Downgrade to webpack version used in our project

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,19 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve("src", "index.js"),
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  optimization: {
-    minimize: false
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['babel-preset-env']
-          }
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['babel-preset-env']
         }
       }
     ]
